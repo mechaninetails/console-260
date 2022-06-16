3b48ba62419101f399fb49a7a37f1ac67f42b596
mechaninetails <107642887+mechaninetails@users.noreply.github.com>
2022-06-16 11:51:08 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+namespace SpriteKind {
+    export const vc = SpriteKind.create()
+    export const cmfg = SpriteKind.create()
+}
+let mySprite4: Sprite = null
+sprites.onOverlap(SpriteKind.cmfg, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    mySprite4.destroy()
+    mySprite4 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(mySprite4, sprites.dungeon.collectibleBlueCrystal)
+})
